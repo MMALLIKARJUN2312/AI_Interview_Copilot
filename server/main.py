9095070d1692047b3b1c35aa2f398655077a4ae7
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-16 22:50:22 +0530

    feat(auth): register auth routes and initialize user table creation

--- server/main.py
+++ server/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 
+from app.db.database import Base
+from app.db.database import engine
+
+from app.models.user import User 
+
+Base.metadata.create_all(
+    bind=engine
+)
+
+from app.api.v1.auth import router as auth_router
+
 app = FastAPI(
     title="AI Interview Copilot",
     version="1.0.0"
 )
+
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
