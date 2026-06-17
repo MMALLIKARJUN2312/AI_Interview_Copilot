a6dd4843c70afcb7d4ab1434cb0200577607de5c
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-17 19:36:18 +0530

    feat(auth): add current user dependency

---
+++ server/app/core/dependencies.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt 
+from app.core.config import Settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token : str = Depends(oauth2_scheme)):
+    try : 
+        payload = jwt.decode(token, Settings.JWT_SECRET, algorithms=[Settings.JWT_ALGORITHM])
+        return payload
+    
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid Token")
+        
